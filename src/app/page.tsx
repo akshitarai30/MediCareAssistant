f2083ec3917a6fdc3ab41f8be47e4614f60e9680
Akshita Rai <raiakshita30@gmail.com>
2025-11-09 15:29:53 +0000

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 'use client';
-export const dynamic = 'force-dynamic';
-import { Suspense } from 'react';
 
 import * as React from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
