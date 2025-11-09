14f571bd7002c07e5559cdef6f374fa5bc5a4e5f
Akshita Rai <raiakshita30@gmail.com>
2025-11-09 14:06:33 +0000

    final commit

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-5891203853-10e6c",
-  "appId": "1:258632602935:web:9cb26c17948ad6f48336b2",
-  "apiKey": "AIzaSyAySP7mbmHp4nTT0XeY0BscasHtiIS8VzA",
-  "authDomain": "studio-5891203853-10e6c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "258632602935"
-};
